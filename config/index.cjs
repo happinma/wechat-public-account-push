83103376d65d44363130525cb72fcd9563874423
happinma <71816056+happinma@users.noreply.github.com>
2022-10-02 11:34:45 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id:'oNaV75nZ650IjDekIYv-VJn49OlU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'ouRRdaI0vSpYJSL1Ws59FdI-JLzC4AoaoyXZpjECRY',
+      useTemplateId: 'LouRRdaI0vSpYJSL1Ws59FdI-JLzC4AoaoyXZpjECRY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-18',
       festivals: [
